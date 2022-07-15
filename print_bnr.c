3ce62ec04074bba5b7bed79c8df1381b14ce5f1d
Dkemzy <105012259+Dkemzy@users.noreply.github.com>
2022-07-15 23:54:55 +0300

    Teamwork

--- print_bnr.c
+++ print_bnr.c
@@ -1,52 +1,42 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
- * print_add - prints the address of an input variable
- * @arguments: input address.
- * @buf: buffer pointer.
+ * print_bnr - prints decimal in binary
+ * @arguments: input string
+ * @buf: buffer pointer
  * @ibuf: index for buffer pointer
- *
  * Return: number of chars printed.
  */
-int print_add(va_list arguments, char *buf, unsigned int ibuf)
+int print_bnr(va_list arguments, char *buf, unsigned int ibuf)
 {
-	void *add;
-	long int int_input;
-	int i, count, first_digit, isnegative;
-	char *hexadecimal, *binary;
-	char nill[] = "(nil)";
+	int int_input, count, i, first_one, isnegative;
+	char *binary;
 
-	add = (va_arg(arguments, void *));
-	if (add == NULL)
-	{
-		for (i = 0; nill[i]; i++)
-			ibuf = handl_buf(buf, nill[i], ibuf);
-		return (5);
-	}
-	int_input = (intptr_t)add;
+	int_input = va_arg(arguments, int);
 	isnegative = 0;
+	if (int_input == 0)
+	{
+		ibuf = handl_buf(buf, '0', ibuf);
+		return (1);
+	}
 	if (int_input < 0)
 	{
 		int_input = (int_input * -1) - 1;
 		isnegative = 1;
 	}
-	binary = malloc(sizeof(char) * (64 + 1));
-	binary = fill_binary_array(binary, int_input, isnegative, 64);
-	hexadecimal = malloc(sizeof(char) * (16 + 1));
-	hexadecimal = fill_hex_array(binary, hexadecimal, 0, 16);
-	ibuf = handl_buf(buf, '0', ibuf);
-	ibuf = handl_buf(buf, 'x', ibuf);
-	for (first_digit = i = count = 0; hexadecimal[i]; i++)
+	binary = malloc(sizeof(char) * (32 + 1));
+	binary = fill_binary_array(binary, int_input, isnegative, 32);
+	first_one = 0;
+	for (count = i = 0; binary[i]; i++)
 	{
-		if (hexadecimal[i] != '0' && first_digit == 0)
-			first_digit = 1;
-		if (first_digit)
+		if (first_one == 0 && binary[i] == '1')
+			first_one = 1;
+		if (first_one == 1)
 		{
-			ibuf = handl_buf(buf, hexadecimal[i], ibuf);
+			ibuf = handl_buf(buf, binary[i], ibuf);
 			count++;
 		}
 	}
 	free(binary);
-	free(hexadecimal);
-	return (count + 2);
+	return (count);
 }
